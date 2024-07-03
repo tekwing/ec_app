f6e3d63433bd279e703b33c81eaffe41b39e407b
tekwing <ajith969818@gmail.com>
2024-07-03 17:37:18 +0530

    calendar updates

--- resources/views/SuperAdmin/ec_meeting.blade.php
+++ resources/views/SuperAdmin/ec_meeting.blade.php
@@ -480,7 +480,7 @@
             </div>
             <div class="modal-footer">
                 <button type="button" class="btn btn-light" data-bs-dismiss="modal">Close</button>
-                <button type="button" class="btn btn-primary ">Save Changes</button>
+                <!-- <button type="button" class="btn btn-primary ">Save Changes</button> -->
             </div>
 
         </div>
